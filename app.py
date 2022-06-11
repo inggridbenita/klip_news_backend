a58968f454d043a03dd945c7b001520a93bb8651
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-06-11 11:30:19 +0700

    Tidak menggunakan file routes

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ import handler as h
 app = Flask(__name__)
 CORS(app)
 
-import routes
+@app.route('/get_all_news')
+def get_all_news():
+    return h.get_all_news_handler()
+
+
+@app.route('/get_news_detail')
+def get_news_detail():
+    news_id = request.args.get('id')
+    return h.get_news_detail_handler(news_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
