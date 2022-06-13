5d23ab1ecd6bb8ded6c5044291899d8b35c73311
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-06-13 11:14:52 +0700

    Menggunakan file routes

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-from flask import Flask, request
-from flask_cors import CORS
-import handler as h
-
-app = Flask(__name__)
-CORS(app)
-
-
-@app.route('/get_all_news')
-def get_all_news():
-    return h.get_all_news_handler()
-
-
-@app.route('/get_news_detail')
-def get_news_detail():
-    news_id = request.args.get('id')
-    return h.get_news_detail_handler(news_id)
+import routes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    routes.app.run(debug=True)
